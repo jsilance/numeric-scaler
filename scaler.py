4a2b93e2387f43d1905e14219ec6a2961b8385ea
jsilance <52762222+jsilance@users.noreply.github.com>
2022-10-29 06:18:45 +0200

    opti
    optimisation framerate

--- scaler.py
+++ scaler.py
@@ -3,7 +3,7 @@ from PIL import ImageGrab
 import numpy as np
 import cv2
 
-SCREEN_SIZE = (1920, 1080)
+# SCREEN_SIZE = (1920, 1080)
 
 module_size = (5*128, 5*64) # output screen size 
 capture_area = (0, 0, 1920, 1080)
@@ -19,13 +19,12 @@ def img_output_calculator(img):
 	img_out = np.array(img)
 	img_out = cv2.resize(img_out, module_size)
 	img_out = cv2.cvtColor(img_out, cv2.COLOR_BGR2RGB)
-	img_out = cv2.copyMakeBorder(img_out, 0, 0, 11, 0, cv2.BORDER_CONSTANT, None, value = 0)
+	img_out = cv2.copyMakeBorder(img_out, 0, 0, 12, 0, cv2.BORDER_CONSTANT, None, value = 0)
 	cv2.imshow('Output', img_out)
-	# cv2.moveWindow('Output', -1940 ,-32)
 	cv2.moveWindow('Output', offset[0]-20, offset[1]-32)
 
 def controls():
-	if cv2.waitKey(10) == ord('q'):
+	if cv2.waitKey(1) == 27:
 		return 1
 
 while True:
